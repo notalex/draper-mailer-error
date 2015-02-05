2a49bed90d1ab35991b9a2159035e6d6b7f21d15
Alex Johnson <notalexjohnson@gmail.com>
2015-02-05 11:05:03 +0530

    Adding missing `from` option for mailer

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ActionMailer::Base
   def send_email
-    mail(to: 'someone@example.com', subject: 'testing draper')
+    mail(to: 'someone@example.com', subject: 'testing draper', from: 'someone_else@example.com')
   end
 end
